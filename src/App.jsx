0993aae4df45ba88d7614e3f01b4c6a2c41f2808
Manal-Mansour <manalgendia@gmail.com>
2025-04-20 20:37:56 +0200

    Add h2 from location

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2> Branch update</h2>
+      <h2>How to merge with master iam written from loc branch</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
