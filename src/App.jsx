c5609eb082ca9595022287eda7a8091320e73ff9
Manal-Mansour <manalgendia@gmail.com>
2025-04-21 15:15:23 +0200

    adding to hub

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,8 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more how are things after
+        uploading in hub
       </p>
     </>
   );
